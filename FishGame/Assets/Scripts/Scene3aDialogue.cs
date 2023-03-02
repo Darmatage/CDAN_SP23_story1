6cd65dfbb32f682c572252d4a6ded855acda4d2e
Katie-Bees <kefogarty@massart.edu>
2023-03-02 14:54:55 -0500

    3a and 3b
    worked on 3a and 3b

--- FishGame/Assets/Scripts/Scene3aDialogue.cs
+++ FishGame/Assets/Scripts/Scene3aDialogue.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 
-public class Scene3bDialogue : MonoBehaviour {
+public class Scene3aDialogue : MonoBehaviour {
         public int primeInt = 1;         // This integer drives game progress!
         public Text Char1name;
         public Text Char1speech;
