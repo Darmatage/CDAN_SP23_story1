ea1d13e403c33082250206a8217d9ea028aea4d2
ashlysalguero <asalguero2@massart.edu>
2023-03-16 00:53:26 -0400

    Finished4b, updated build settings, final changes to End lose 4

--- FishGame/Assets/Scripts/Scene4bDialogue.cs
+++ FishGame/Assets/Scripts/Scene4bDialogue.cs
@@ -88,7 +88,18 @@ public void next(){
                 Char2speech.text = "Yo! Watch this!";
                 //gameHandler.AddPlayerStat(1);
         }
-       else if (primeInt == 4){
+		
+		       else if (primeInt ==4){
+                ArtChar2a.SetActive(false);
+				ArtChar2d.SetActive(true);
+				Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "*The dolphin shows you their epic tricks!*";
+                //gameHandler.AddPlayerStat(1);
+        }
+		
+       else if (primeInt == 5){
                 ArtChar1d.SetActive(false);
 				ArtChar1b.SetActive(true);
 				Char1name.text = "Fish";
@@ -96,7 +107,7 @@ public void next(){
                 Char2name.text = "";
                 Char2speech.text = "";
         }
-       else if (primeInt == 5){
+       else if (primeInt == 6){
                 ArtChar2d.SetActive(false);
 				ArtChar2a.SetActive(true);
 				Char1name.text = "";
@@ -105,13 +116,13 @@ public void next(){
                 Char2speech.text = "You like my moves?";
                 //gameHandler.AddPlayerStat(1);
         }
-       else if (primeInt == 6){
+       else if (primeInt == 7){
                 Char1name.text = "Fish";
                 Char1speech.text = "Sure! You should be the entertainment at my party!";
                 Char2name.text = "";
                 Char2speech.text = "";
         }
-       else if (primeInt ==7){
+       else if (primeInt ==8){
                 ArtChar2a.SetActive(false);
 				ArtChar2b.SetActive(true);
 				Char1name.text = "";
@@ -119,20 +130,20 @@ public void next(){
                 Char2name.text = "Dolphin";
                 Char2speech.text = "Entertainment?";
         }
-       else if (primeInt == 8){
+       else if (primeInt == 9){
                 Char1name.text = "Fish";
                 Char1speech.text = "Yeah, do your little dances at my party! I’m sure everyone will love it!";
                 Char2name.text = "";
                 Char2speech.text = "";
                
 		 }
-       else if (primeInt == 9){
+       else if (primeInt == 10){
                 ArtChar2b.SetActive(false);
 				ArtChar2c.SetActive(true);
 				Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "Dolphin";
-                Char2speech.text = "Y’know I could teach you these tricks if you’d like. It won’t take long…";
+                Char2speech.text = "Y’know I could teach you these tricks if you’d like…";
 
 			   // Turn off "Next" button, turn on "Choice" buttons
                 nextButton.SetActive(false);
@@ -143,8 +154,8 @@ public void next(){
 
 // ENCOUNTER AFTER CHOICE #1
        else if (primeInt == 100){
-             
-
+             ArtChar1e.SetActive(false);
+			 ArtChar1b.SetActive(true);
 			 Char1name.text = "Fish";
                 Char1speech.text = "I really did admire your moves!";
                 Char2name.text = "";
@@ -159,7 +170,9 @@ public void next(){
                 Char2speech.text = "It didn’t sound like you did…";
         }
 	   else if (primeInt == 102){
-               ArtChar2b.SetActive(false);
+               ArtChar1b.SetActive(false);
+				ArtChar1a.SetActive(true);
+			   ArtChar2b.SetActive(false);
 			   ArtChar2a.SetActive(true);
 			 Char1name.text = "Fish";
                 Char1speech.text = " Oh but I did! I’ve never seen anything as majestic as your performance!";
@@ -175,7 +188,9 @@ public void next(){
                 Char2speech.text = "Wait! I uh…I wouldn’t mind going to your party…";
         }
 	   else if (primeInt == 104){
-                ArtChar2b.SetActive(false);
+                ArtChar1a.SetActive(false);
+				ArtChar1b.SetActive(true);
+				ArtChar2b.SetActive(false);
 			   ArtChar2a.SetActive(true);
 				Char1name.text = "Fish";
                 Char1speech.text = "Really!? Neat-o! See ya at my party then!";
@@ -186,17 +201,19 @@ public void next(){
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "Dolphin";
-                Char2speech.text = "Goobye, fish.";
+                Char2speech.text = "Goobye, Fish.";
         }
 	   
 	   else if (primeInt == 106){
-                Char1name.text = "Fish";
+                ArtChar2a.SetActive(false);
+				Char1name.text = "Fish";
                 Char1speech.text = "Where to next?";
                 Char2name.text = "";
                 Char2speech.text = "";
                 nextButton.SetActive(false);
                 allowSpace = false;
                 NextScene1Button.SetActive(true);
+				NextScene2Button.SetActive(true);
         }
 
        else if (primeInt == 200){
@@ -225,21 +242,33 @@ public void next(){
         }
 	   
 	   else if (primeInt == 204){
-                Char1name.text = "";
+                ArtChar1e.SetActive(false);
+				Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "Dolphin";
-                Char2speech.text = "This ain't some circus. I won’t go to your party! You're somefish who doesn’t appreciate true art!";
-                nextButton.SetActive(false);
+                Char2name.text = "";
+                Char2speech.text = "*The Dolphin tosses fish out of water.*";
+                nextButton.SetActive(true);
                 allowSpace = false;
-                NextScene2Button.SetActive(true);
+                
         }
 
+       else if (primeInt == 205){
+                Char1name.text = "";
+                Char1speech.text = "";
+				nextButton.SetActive(false);
+				//NextScene3Button.SetActive(true);
+		SceneManager.LoadScene("End_Lose4");
+                // Char2name.text = "";
+                // Char2speech.text = "";
+        }
       //Please do NOT delete this MOST HOLY bracket that ends the next() function:
      }
 
 // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch-scenes)
         public void Choice1aFunct(){
-                Char1name.text = "Fish";
+                ArtChar1b.SetActive(false);
+				ArtChar1e.SetActive(true);
+				Char1name.text = "Fish";
                 Char1speech.text = "Darn, that’s just too bad.";
                 Char2name.text = "";
                 Char2speech.text = "";
@@ -250,7 +279,9 @@ public void next(){
                 allowSpace = true;
         }
         public void Choice1bFunct(){
-                Char1name.text = "Fish";
+                ArtChar1b.SetActive(false);
+				ArtChar1e.SetActive(true);
+				Char1name.text = "Fish";
                 Char1speech.text = "Oi! Why did you hit me?";
                 Char2name.text = "";
                 Char2speech.text = "";
@@ -262,9 +293,12 @@ public void next(){
         }
 
         public void SceneChange1(){
-               SceneManager.LoadScene("Scene2a");
+               SceneManager.LoadScene("Scene5a");
         }
         public void SceneChange2(){
-                SceneManager.LoadScene("Scene2b");
+                SceneManager.LoadScene("Scene5b");
+        }
+		public void SceneChange3(){
+                SceneManager.LoadScene("End_Lose3");
         }
 }
