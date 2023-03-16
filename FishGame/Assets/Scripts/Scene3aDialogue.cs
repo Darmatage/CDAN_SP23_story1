27871e271d997d090008cc000e01b6e173bc6e96
Katie-Bees <kefogarty@massart.edu>
2023-03-15 23:53:31 -0400

    finished dialog and buttons on 3a

--- FishGame/Assets/Scripts/Scene3aDialogue.cs
+++ FishGame/Assets/Scripts/Scene3aDialogue.cs
@@ -28,6 +28,8 @@ public class Scene3aDialogue : MonoBehaviour {
         public GameObject ArtBG1;
         public GameObject Choice1a;
         public GameObject Choice1b;
+		public GameObject Choice2a;
+        public GameObject Choice2b;
         public GameObject NextScene1Button;
         public GameObject NextScene2Button;
         public GameObject nextButton;
@@ -42,6 +44,8 @@ void Start(){
         ArtBG1.SetActive(true);
         Choice1a.SetActive(false);
         Choice1b.SetActive(false);
+		Choice2a.SetActive(false);
+        Choice2b.SetActive(false);
         NextScene1Button.SetActive(false);
         NextScene2Button.SetActive(false);
         nextButton.SetActive(true);
@@ -117,11 +121,19 @@ public void next(){
 				Char3name.text = "A deep, powerful voice";
                 Char3speech.text = "A PARTY?";
         }
-       else if (primeInt == 8){
+		else if (primeInt ==8){
+                Char1name.text = "";
+                Char1speech.text = "oH fUCc it's a  S H A R K";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "";
+                Char3speech.text = "";
+        }
+		else if (primeInt ==9){
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "Do not play the stupid. You will take me to him.";
+                Char2name.text = "Fish";
+                Char2speech.text = "Yeah!  It’s gonna be a reeeal shindig!  You in?";
 				Char3name.text = "";
                 Char3speech.text = "";
                 // Turn off "Next" button, turn on "Choice" buttons
@@ -131,44 +143,139 @@ public void next(){
                 Choice1b.SetActive(true); // function Choice1bFunct()
         }
 
-// ENCOUNTER AFTER CHOICE #1
+// ENCOUNTER AFTER CHOICE #1A
        else if (primeInt == 100){
+		        Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "Then you are no use to me, and must be silenced.";
-				Char3name.text = "";
-                Char3speech.text = "";
-        }
-       else if (primeInt == 101){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "Come back here! Do not think you can hide from me!";
-				Char3name.text = "";
-                Char3speech.text = "";
-                nextButton.SetActive(false);
-                allowSpace = false;
-                NextScene1Button.SetActive(true);
-        }
-
-       else if (primeInt == 200){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "Do not think you can fool me, human. Where will we find him?";
-				Char3name.text = "";
-                Char3speech.text = "";
-        }
-       else if (primeInt == 201){
-                Char1name.text = "YOU";
-                Char1speech.text = "Ragu hangs out in a rough part of town. I'll take you now.";
                 Char2name.text = "";
                 Char2speech.text = "";
 				Char3name.text = "";
                 Char3speech.text = "";
+				nextButton.SetActive(false);
+				Choice2a.SetActive(true);
+                Choice2b.SetActive(true);
+                allowSpace = true;
+        }
+// ENCOUNTER AFTER CHOICE #1B
+       else if (primeInt == 200){
+		        Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "Shark";
+                Char3speech.text = "YES THAT IS TRUE LITTLE ONE. I VERY MUCH ENJOY A GOOD OLD FASHIONED HOEDOWN. I WILL MOST DEFINITELY BE THERE FOR THE OCCASION.";
+        }
+		       else if (primeInt == 201){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Fish";
+                Char2speech.text = "See?  What did I tell you?  We will see you there Mr. Shark!";
+				Char3name.text = "";
+                Char3speech.text = "";
+        }
+		       else if (primeInt == 202){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "Shark";
+                Char3speech.text = "AND I YOU.";
+        }
+
+       else if (primeInt == 203){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Fish";
+                Char2speech.text = "Alrighty!  Where to next?";
+				Char3name.text = "";
+                Char3speech.text = "";
                 nextButton.SetActive(false);
                 allowSpace = false;
+				NextScene1Button.SetActive(true);
+                NextScene2Button.SetActive(true);
+        }
+// ENCOUNTER AFTER CHOICE #2A
+else if (primeInt == 300){
+		        Choice2a.SetActive(false);
+                Choice2b.SetActive(false);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "Shark";
+                Char3speech.text = "THAT IS QUITE ALL RIGHT LITTLE ONE. I WILL MOST DEFINITELY BE THERE FOR THE OCCASION.";
+        }
+		       else if (primeInt == 301){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Fish";
+                Char2speech.text = "See?  What did I tell you?  We will see you there Mr. Shark!";
+				Char3name.text = "";
+                Char3speech.text = "";
+        }
+		       else if (primeInt == 302){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "Shark";
+                Char3speech.text = "AND I YOU.";
+        }
+
+               else if (primeInt == 303){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Fish";
+                Char2speech.text = "Alrighty!  Where to next?";
+				Char3name.text = "";
+                Char3speech.text = "";
+                nextButton.SetActive(false);
+                allowSpace = false;
+				NextScene1Button.SetActive(true);
+                NextScene2Button.SetActive(true);
+        }
+// ENCOUNTER AFTER CHOICE #2B
+else if (primeInt == 400){
+		        Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "Shark";
+                Char3speech.text = "YES THAT IS TRUE LITTLE ONE. I VERY MUCH ENJOY A GOOD OLD FASHIONED HOEDOWN. I WILL MOST DEFINITELY BE THERE FOR THE OCCASION.";
+        }
+		       else if (primeInt == 401){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Fish";
+                Char2speech.text = "See?  What did I tell you?  We will see you there Mr. Shark!";
+				Char3name.text = "";
+                Char3speech.text = "";
+        }
+		       else if (primeInt == 402){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+				Char3name.text = "Shark";
+                Char3speech.text = "AND I YOU.";
+        }
+
+               else if (primeInt == 403){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Fish";
+                Char2speech.text = "Alrighty!  Where to next?";
+				Char3name.text = "";
+                Char3speech.text = "";
+                nextButton.SetActive(false);
+                allowSpace = false;
+				NextScene1Button.SetActive(true);
                 NextScene2Button.SetActive(true);
         }
 
@@ -177,23 +284,23 @@ public void next(){
 
 // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch-scenes)
         public void Choice1aFunct(){
-                Char1name.text = "YOU";
-                Char1speech.text = "I don't know what you're talking about!";
-                Char2name.text = "";
-                Char2speech.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Fish";
+                Char2speech.text = "What do you mean?";
 				Char3name.text = "";
                 Char3speech.text = "";
                 primeInt = 99;
                 Choice1a.SetActive(false);
                 Choice1b.SetActive(false);
-                nextButton.SetActive(true);
-                allowSpace = true;
+				nextButton.SetActive(true);
+                allowSpace = false;
+
         }
         public void Choice1bFunct(){
-                Char1name.text = "YOU";
-                Char1speech.text = "Sure, anything you want... just lay off the club.";
-                Char2name.text = "";
-                Char2speech.text = "";
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Fish";
+                Char2speech.text = "Yeah! Sharks love a good party, isn't that right?";
 				Char3name.text = "";
                 Char3speech.text = "";
                 primeInt = 199;
@@ -201,7 +308,36 @@ public void next(){
                 Choice1b.SetActive(false);
                 nextButton.SetActive(true);
                 allowSpace = true;
+				
         }
+		public void Choice2aFunct(){
+                Char1speech.text = "";
+                Char2name.text = "Fish";
+                Char2speech.text = "Whaaaat? Come on now, don't be so discriminatory. I’m sorry Mr. Shark, my friend here has no idea what they’re talking about.";
+				Char3name.text = "";
+                Char3speech.text = "";
+                primeInt = 299;
+                Choice2a.SetActive(false);
+                Choice2b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+        public void Choice2bFunct(){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Fish";
+                Char2speech.text = "AAAAAAAAAAAAAAAAA";
+				Char3name.text = "";
+                Char3speech.text = "";
+                primeInt = 399;
+                Choice2a.SetActive(false);
+                Choice2b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+				NextScene1Button.SetActive(true);
+                NextScene2Button.SetActive(true);
+        }
+
 
         public void SceneChange1(){
                SceneManager.LoadScene("Scene2a");
